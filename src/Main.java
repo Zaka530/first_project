dd377d3618adeca3add4ad9dc300385cb315c732
kamron <kbariev7@gmail.com>
2024-03-13 18:43:24 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        ArrayList arrayList=new ArrayList ();
+        arrayList.add(55);
+        arrayList.add(22);
+        arrayList.add(50);
+        System.out.println(arrayList);
+        arrayList.remove(0);
+        System.out.println(arrayList);
+
+    }
+}
+
+
